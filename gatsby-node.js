793764751f4946318a70bed051864e9b48e97269
ayan4m1 <andrew@bulletlogic.com>
2019-08-24 11:35:42 -0400

    add dynamic page generation from markdown files

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,42 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const { resolve } = require('path');
 
-// You can delete this file if you're not using it
+exports.createPages = async ({ actions, graphql, reporter }) => {
+  const component = resolve('src/pages/content.js');
+  const { createPage } = actions;
+  const result = await graphql(`
+    {
+      allMarkdownRemark(limit: 1000) {
+        edges {
+          node {
+            html
+            frontmatter {
+              path
+              title
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  if (result.errors) {
+    reporter.panicOnBuild('Error while running GraphQL markdown query.');
+    return;
+  }
+
+  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    const {
+      html,
+      frontmatter: { path, title }
+    } = node;
+
+    createPage({
+      context: {
+        html,
+        title
+      },
+      component,
+      path
+    });
+  });
+};
---
+++ src/pages/content.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+import PropTypes from 'prop-types';
+import { Container, Row, Col } from 'react-bootstrap';
+
+import SEO from '../components/seo';
+import Layout from '../components/layout';
+
+const Content = ({ data }) => {
+  const {
+    markdownRemark: {
+      html,
+      frontmatter: { title }
+    }
+  } = data;
+
+  return (
+    <Layout>
+      <SEO title={title} />
+      <Container className="mt-5">
+        <Row>
+          <Col md="12" dangerouslySetInnerHTML={{ __html: html }} />
+        </Row>
+      </Container>
+    </Layout>
+  );
+};
+
+Content.displayName = 'Content';
+Content.propTypes = {
+  data: PropTypes.object
+};
+
+export default Content;
+
+export const pageQuery = graphql`
+  query($path: String!) {
+    markdownRemark(frontmatter: { path: { eq: $path } }) {
+      html
+      frontmatter {
+        path
+        title
+      }
+    }
+  }
+`;
