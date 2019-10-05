b5ede59d7e22f31819fe9bb801b11c0dcbbf38bc
ayan4m1 <andrew@bulletlogic.com>
2019-10-04 20:50:02 -0400

    build markdown and wordpress pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,7 @@
 const { resolve } = require('path');
 
-exports.createPages = async ({ actions, graphql, reporter }) => {
-  const component = resolve('src/components/content.js');
+const createMarkdownPages = async ({ actions, graphql, reporter }) => {
+  const component = resolve('src/components/content/markdown.js');
   const { createPage } = actions;
   const result = await graphql(`
     {
@@ -39,4 +39,54 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
       path
     });
   });
+};
+
+const createWordPressPages = async ({ actions, graphql, reporter }) => {
+  const component = resolve('src/components/content/wordpress.js');
+  const { createPage } = actions;
+  const result = await graphql(`
+    {
+      allWordpressPost {
+        edges {
+          node {
+            id
+            path
+            title
+            status
+            content
+            excerpt
+            date
+            modified
+          }
+        }
+      }
+    }
+  `);
+
+  if (result.errors) {
+    reporter.panicOnBuild('Error while running GraphQL wordpress query.');
+    return;
+  }
+
+  result.data.allWordpressPost.edges.forEach(({ node }) => {
+    const { content, id, path, slug, title } = node;
+
+    // eslint-disable-next-line
+    console.dir(path);
+    createPage({
+      context: {
+        content,
+        title,
+        slug,
+        id
+      },
+      component,
+      path
+    });
+  });
+};
+
+exports.createPages = async options => {
+  await createMarkdownPages(options);
+  await createWordPressPages(options);
 };
