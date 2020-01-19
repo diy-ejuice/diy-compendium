4a085d359203a07d58e5aacc3d1a8539f9fb8021
ayan4m1 <andrew@bulletlogic.com>
2020-01-19 12:05:58 -0500

    remove all references to WP

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,20 +23,6 @@ module.exports = {
         path: `${__dirname}/src/docs`
       }
     },
-    {
-      resolve: 'gatsby-source-wordpress',
-      options: {
-        baseUrl: process.env.WP_BASE_URL,
-        protocol: 'https',
-        useACF: false,
-        auth: {
-          /* eslint-disable camelcase */
-          jwt_user: process.env.WP_JWT_USER,
-          jwt_pass: process.env.WP_JWT_PASSWORD
-          /* eslint-enable camelcase */
-        }
-      }
-    },
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
     {
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,4 @@
 const { resolve } = require('path');
-const omit = require('lodash.omit');
 
 const createMarkdownPages = async ({ actions, graphql, reporter }) => {
   const component = resolve('src/components/content/markdown.js');
@@ -42,96 +41,8 @@ const createMarkdownPages = async ({ actions, graphql, reporter }) => {
   });
 };
 
-const createWordPressPages = async ({ actions, graphql, reporter }) => {
-  const postComponent = resolve('src/components/content/wordpress/post.js');
-  const pageComponent = resolve('src/components/content/wordpress/page.js');
-  const { createPage } = actions;
-  const result = await graphql(`
-    {
-      allWordpressPost(limit: 1000) {
-        edges {
-          node {
-            id
-            path
-            title
-            status
-            content
-            excerpt
-            date
-            modified
-            author {
-              id
-              name
-              avatar_urls {
-                wordpress_24
-                wordpress_48
-                wordpress_96
-              }
-            }
-          }
-        }
-      }
-
-      allWordpressPage(limit: 1000) {
-        edges {
-          node {
-            id
-            path
-            title
-            status
-            content
-            excerpt
-            date
-            modified
-            author {
-              id
-              name
-              avatar_urls {
-                wordpress_24
-                wordpress_48
-                wordpress_96
-              }
-            }
-          }
-        }
-      }
-    }
-  `);
-
-  if (result.errors) {
-    reporter.panicOnBuild('Error while running GraphQL wordpress query.');
-    return;
-  }
-
-  const postFromNodes = ({ node }) => {
-    const { id, path } = node;
-
-    reporter.info(`Mapped ${id} to ${path}`);
-    createPage({
-      context: omit(node, ['path']),
-      component: postComponent,
-      path
-    });
-  };
-
-  const pageFromNodes = ({ node }) => {
-    const { id, path } = node;
-
-    reporter.info(`Mapped ${id} to ${path}`);
-    createPage({
-      context: omit(node, ['path']),
-      component: pageComponent,
-      path
-    });
-  };
-
-  result.data.allWordpressPost.edges.forEach(postFromNodes);
-  result.data.allWordpressPage.edges.forEach(pageFromNodes);
-};
-
 exports.createPages = async options => {
   await createMarkdownPages(options);
-  await createWordPressPages(options);
 };
 
 exports.onCreateWebpackConfig = ({ actions }) => {
