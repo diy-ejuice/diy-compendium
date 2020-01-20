e30fc006b9d92d3c04a64a60d465298f3b64f55a
ayan4m1 <andrew@bulletlogic.com>
2020-01-20 14:42:53 -0500

    replace index.js content

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,13 @@ const IndexPage = () => (
       <Row>
         <Col md="12">
           <h1>Welcome to the DIY Compendium!</h1>
-          <p>We need some content here.</p>
+          <p>
+            This site attempts to compile the highest quality documentation
+            available for all aspects of DIY e-liquid mixing. It is managed and
+            features content contributed by volunteers from the DIY community.
+            We&apos;ll have featured content here soon, but for now you can
+            access all of the articles using the navbar above.
+          </p>
         </Col>
       </Row>
     </Container>
