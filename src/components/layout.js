a7430e4fc8a0f40ca771bce5126f11d0c947b919
ayan4m1 <andrew@bulletlogic.com>
2024-09-19 00:29:12 -0400

    chore: remove no-op SSRProvider

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,6 @@
+import { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { useStaticQuery, graphql } from 'gatsby';
-import { SSRProvider } from 'react-bootstrap';
 
 import Footer from 'components/footer';
 import Header from 'components/header';
@@ -17,11 +17,11 @@ export default function Layout({ children }) {
   `);
 
   return (
-    <SSRProvider>
+    <Fragment>
       <Header siteTitle={data.site.siteMetadata.title} />
       <main className="mt-3">{children}</main>
       <Footer />
-    </SSRProvider>
+    </Fragment>
   );
 }
 
